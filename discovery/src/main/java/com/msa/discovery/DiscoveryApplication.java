082265c348a1285b60e187e1671baf4a0809e04e
jihwan2da <wlghks8982@naver.com>
2023-05-10 19:07:20 +0900

    #1 : Service Discovery 서버 구축

--- discovery/src/main/java/com/msa/discovery/DiscoveryApplication.java
+++ discovery/src/main/java/com/msa/discovery/DiscoveryApplication.java
@@ -2,8 +2,15 @@ package com.msa.discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @EnableEurekaServer
+ * 유레카 서버로서의 역할 지정
+ */
 
 @SpringBootApplication
+@EnableEurekaServer
 public class DiscoveryApplication {
 
     public static void main(String[] args) {
