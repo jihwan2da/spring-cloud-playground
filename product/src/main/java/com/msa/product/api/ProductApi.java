34be2155183569a9747e9beb23d5a4bc46df91de
jihwan2da <wlghks8982@naver.com>
2023-06-01 00:35:59 +0900

    feat: Product MicroService HealthCheck API Implementation

---
+++ product/src/main/java/com/msa/product/api/ProductApi.java
@@ -0,0 +1,16 @@
+package com.msa.product.api;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/product-service")
+public class ProductApi {
+
+    @GetMapping("/health_check")
+    public String healthCheck(HttpServletRequest request) {
+        return String.format("It's Working in User Service on Port %s", request.getServerPort());
+    }
+}
