c915277d26efb8d7f876dbbba219a9ffd6212ad9
jihwan2da <wlghks8982@naver.com>
2023-08-15 02:04:04 +0900

    feat: SignUp Presentation layer Impl

--- user/src/main/java/com/msa/user/users/presentation/UserApi.java
+++ user/src/main/java/com/msa/user/users/presentation/UserApi.java
@@ -1,17 +1,34 @@
-package com.msa.user.presentation;
+package com.msa.user.users.presentation;
 
+import com.msa.user.users.application.UserService;
+import com.msa.user.users.presentation.dto.UserRequest;
 import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
+import static com.msa.user.users.presentation.dto.UserRequest.*;
+import static com.msa.user.users.presentation.dto.UserResponse.*;
+
 @RestController
-@RequestMapping("/user-service")
+@RequiredArgsConstructor
+@RequestMapping("/user-service/api/v1/users")
 public class UserApi {
 
+    private final UserService userService;
+
     @GetMapping("/health_check")
-    public Mono<String> healthCheck(HttpServletRequest request) {
-        return Mono.just(String.format("It's Working in User Service on Port %s", request.getServerPort()));
+    public Mono<String> healthCheck(ServerWebExchange exchange) {
+        ServerHttpRequest request = exchange.getRequest();
+        return Mono.just(String.format("It's Working in User Service on Port %s", request.getURI().getPort()));
+    }
+
+    @PostMapping("/signup")
+    public Mono<ResponseEntity<UserInfo>> signUp(@RequestBody Mono<SignUp> request) {
+        return userService.signUp(request);
     }
 }
