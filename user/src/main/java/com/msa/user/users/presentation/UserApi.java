c5bc9dc6ad14d31aaeb9f1e68b7e98fe542acaf4
jihwan2da <wlghks8982@naver.com>
2023-08-14 23:24:05 +0900

    chore: user service webflux 설정

--- user/src/main/java/com/msa/user/users/presentation/UserApi.java
+++ user/src/main/java/com/msa/user/users/presentation/UserApi.java
@@ -1,16 +1,17 @@
-package com.msa.user.api;
+package com.msa.user.presentation;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/user-service")
 public class UserApi {
 
     @GetMapping("/health_check")
-    public String healthCheck(HttpServletRequest request) {
-        return String.format("It's Working in User Service on Port %s", request.getServerPort());
+    public Mono<String> healthCheck(HttpServletRequest request) {
+        return Mono.just(String.format("It's Working in User Service on Port %s", request.getServerPort()));
     }
 }
